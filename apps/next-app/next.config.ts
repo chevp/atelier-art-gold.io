295f62ac5806b6d86c09043909a44b024abb35ac
chevp <chevillatp@gmail.com>
2025-04-26 14:59:55 +0200

    added home and course pages

--- apps/next-app/next.config.ts
+++ apps/next-app/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: '/atelier-art-gold.io',
-  assetPrefix: '/atelier-art-gold.io',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 export default nextConfig;
