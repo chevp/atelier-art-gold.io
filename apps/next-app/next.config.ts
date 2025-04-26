af07fedf5c2fa4c1e2ad2e34c5ac39b03743301f
chevp <chevillatp@gmail.com>
2025-04-26 14:13:38 +0200

    modified root folder

--- apps/next-app/next.config.ts
+++ apps/next-app/next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/atelier-art-gold.io',
+  assetPrefix: '/atelier-art-gold.io',
 };
 
 export default nextConfig;
+
