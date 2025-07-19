6356342fce38e5637695d8c688c7ee60d0d05a48
chevp <78725490+chevp@users.noreply.github.com>
2025-07-19 11:58:09 +0200

    Update page.tsx

--- webapp.next/src/app/page.tsx
+++ webapp.next/src/app/page.tsx
@@ -180,14 +180,6 @@ export default function Home() {
           chevigold@bluemail.ch
         </p>
 
-        <h3>Wo Sie das Atelier finden:</h3>
-        <iframe
-          width="100%"
-          height="320"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10778.34886268308!2d7.696313399999999!3d47.51742959999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4791b610dd151ff3%3A0x19b9f7f7155e9226!2sVereinshausstrasse+18%2C+4133+Pratteln!5e0!3m2!1sde!2sch!4v1440673794715"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
       </section>
 
     </main>
